eebc7eaf5bfe98168ddebb74568fe45c986e4c59
umetsu <prunusmume0727@gmail.com>
2021-05-09 14:01:25 +0900

    :wrench: code format

--- js/plugins.js
+++ js/plugins.js
@@ -1,13 +1,32 @@
 // Avoid `console` errors in browsers that lack a console.
-(function() {
+(function () {
   let method;
-  const noop = function () {
-  };
+  const noop = function () {};
   const methods = [
-    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    "assert",
+    "clear",
+    "count",
+    "debug",
+    "dir",
+    "dirxml",
+    "error",
+    "exception",
+    "group",
+    "groupCollapsed",
+    "groupEnd",
+    "info",
+    "log",
+    "markTimeline",
+    "profile",
+    "profileEnd",
+    "table",
+    "time",
+    "timeEnd",
+    "timeline",
+    "timelineEnd",
+    "timeStamp",
+    "trace",
+    "warn",
   ];
   let length = methods.length;
   const console = (window.console = window.console || {});
@@ -20,6 +39,6 @@
       console[method] = noop;
     }
   }
-}());
+})();
 
 // Place any jQuery/helper plugins in here.
